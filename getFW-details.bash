ee4db27d476a4f4d61ebfc154e95e40996d3e7bb
Thomas Fay <thomas.fay@esss.se>
2020-02-03 14:22:08 +0100

    First working version.

--- getFW-details.bash
+++ getFW-details.bash
@@ -2,12 +2,13 @@
 usage() {
   echo "Usage: $0" 
 }
+
 exit_abnormal() {
   usage
   exit 1
 }
 
-outFile=/home/iocuser/tfay/versionsFW
+outFile=/home/iocuser/tfay/checkFW/versionsFW
 master=""
 slave_start=""
 slave_end=""
@@ -26,11 +27,21 @@ while getopts "m:s:e:" options; do
   esac
 done
 
+outFile="$outFile"_master-$master
 echo "Firmware versions for slaves of master number [$master]" > "$outFile"
 
-for ((i=3 ; i<$slave_end ; i++)); do
-  cmd="ethercat -m $master upload -p $i -t string 0x1008 0x0"
-  name=$($cmd)
-  echo "Slave $i, $name" | tee -a "$outFile"
+nameReg="0x1008 0x0"
+hwReg="0x1009 0x0"
+fwReg="0x100a 0x0"
+for ((i=0 ; i<="$slave_end" ; i++)); do
+  echo "Slave $i..."
+  cmd="ethercat -m $master upload -p $i -t string"
+  cmdName="$cmd $nameReg"
+  cmdHW="$cmd $hwReg"
+  cmdFW="$cmd $fwReg"
+  name=$($cmdName)
+  version_HW=$($cmdHW)
+  version_FW=$($cmdFW)
+  echo "Slave $i, $name HW:$version_HW FW:$version_FW" >> $outFile
 done
 
