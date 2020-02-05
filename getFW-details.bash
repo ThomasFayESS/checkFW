19ce46dc24ed62f999b26c56920746a1b1e5dfc5
Thomas Fay <thomas.fay@esss.se>
2020-02-05 18:21:41 +0100

    WIP

--- getFW-details.bash
+++ getFW-details.bash
@@ -27,7 +27,10 @@ while getopts "m:s:e:" options; do
   esac
 done
 
-cmdNames="ethercat -m 2 slaves"
+outFile="$outFile"_master-$master
+echo "Firmware versions for slaves of master number [$master]" > "$outFile"
+
+cmdNames="ethercat -m $master slaves"
 stringNames=$($cmdNames)
 regHW='0x1009 0x0'
 regFW='0x100a 0x0'
@@ -36,33 +39,15 @@ echo "$cmdSlaveNames"
 while IFS= read -r line; do
   name=${line#*+  }
   slaveID=${line%0:*}
-  if [[ $name = "EL9*" ]]; then
-    echo "$slaveID: $name No SDOs available for this module."
+  if [[ $name == *"EL9"* ]] || [[ $name == *"EK1"* ]] \
+  || [[ $name == *"EL2624"* ]] || [[ $name == *"EL1809"* ]] \
+  || [[ $name == *"EL3202-0010"* ]]; then
+    echo "$slaveID: $name: No SDOs available for this module." >> $outFile
   else
     cmdHW="ethercat -m $master upload -p $slaveID -t string $regHW"
     ID_HW=$($cmdHW)
     cmdFW="ethercat -m $master upload -p $slaveID -t string $regFW"
     ID_SW=$($cmdFW)
-    echo "$slaveID: $name ID-HW: $ID_HW ID-SW: $ID_SW"
+    echo "$slaveID: $name ID-HW: $ID_HW ID-SW: $ID_SW" >> $outFile
   fi
 done <<< "$stringNames"
-
-exit 1
-outFile="$outFile"_master-$master
-echo "Firmware versions for slaves of master number [$master]" > "$outFile"
-
-nameReg="0x1008 0x0"
-hwReg="0x1009 0x0"
-fwReg="0x100a 0x0"
-for ((i=0 ; i<="$slave_end" ; i++)); do
-  echo "Slave $i..."
-  cmd="ethercat -m $master upload -p $i -t string"
-  cmdName="$cmd $nameReg"
-  cmdHW="$cmd $hwReg"
-  cmdFW="$cmd $fwReg"
-  name=$($cmdName)
-  version_HW=$($cmdHW)
-  version_FW=$($cmdFW)
-  echo "Slave $i, $name HW:$version_HW FW:$version_FW" >> $outFile
-done
-
